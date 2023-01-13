6037cd808d4cce87856eaaf3dea3dd72f5d29bca
qpyracuk <qpyracuk@gmail.com>
2023-01-13 11:10:32 +0300

    add declare

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+declare module "dewart-console-table" {
+	/**
+	 * Creates a table in the console
+	 */
+    declare class Table {
+	/**
+	 * Based on data from an array of classes, returns a row containing a table
+	 * @param {Array<string>} classes Class array
+	 * @returns {String} Line containing the table
+	 */
+	static getOfClasses():string
+	/**
+	 * Based on the data from the class array, displays a table in the console
+	 * @param {Array<string>} classes Class array
+	 */
+	static showOfClasses():void
+	/**
+	 * Based on a two-dimensional array, output returns a string containing a table
+	 * @param {Array<string>} array Table content
+	 * @param {Array<string>} headers Column headings
+	 * @returns {String} Line containing the table
+	 */
+	static getOfArray(array:Array<string>, headers:Array<string>):string
+
+	/**
+	 * Based on a two-dimensional array, outputs a table to the console
+	 * @param {Array<string>} array Table content
+	 * @param {Array<string>} headers Column headings
+	 */
+	static showOfArray(array:Array<string>, headers:Array<string>):void	
+	}
+    export default Table;
+}
--- index.js
+++ index.js
@@ -1,44 +1,56 @@
 import { Console } from 'console';
 import { Transform } from 'stream';
-
-export class Table {
-	static getOfMatrix(matrix, headers) {
-		return Table._generate(Table._matrixToClasses(matrix, headers));
-	}
-
+export default class Table {
+	/**
+	 * Based on data from an array of classes, returns a row containing a table
+	 * @param {Array<string>} classes Class array
+	 * @returns {String} Line containing the table
+	 */
 	static getOfClasses(classes) {
 		return Table._generate(classes);
 	}
-
-	static showOfMatrix(matrix, headers) {
-		console.log(Table._generate(Table._matrixToClasses(matrix, headers)));
-	}
-
+	/**
+	 * Based on the data from the class array, displays a table in the console
+	 * @param {Array<string>} classes Class array
+	 */
 	static showOfClasses(classes) {
 		console.log(Table._generate(classes));
 	}
-
-	static _matrixToClasses(matrix, headers){
-		let rows = matrix.length
-		if (rows === 0) throw Error('empty matrix')
+	/**
+	 * Based on a two-dimensional array, output returns a string containing a table
+	 * @param {Array<string>} array Table content
+	 * @param {Array<string>} headers Column headings
+	 * @returns {String} Line containing the table
+	 */
+	static getOfArray(array, headers) {
+		return Table._generate(Table._arrayToClasses(array, headers));
+	}
+	/**
+	 * Based on a two-dimensional array, outputs a table to the console
+	 * @param {Array<string>} array Table content
+	 * @param {Array<string>} headers Column headings
+	 */
+	static showOfArray(array, headers) {
+		console.log(Table._generate(Table._arrayToClasses(array, headers)));
+	}
+	static _arrayToClasses(array, headers){
+		let rows = array.length
+		if (rows === 0) throw Error('empty array')
 		let cols = 0
 		for (let i = 0; i < rows; i++){
-			cols = (cols < matrix[i].length ? matrix[i].length : cols)
+			cols = (cols < array[i].length ? array[i].length : cols)
 		}
-		console.log(cols);
 		let json = '{"table":['
 		for (let i = 0 ; i < rows; i++){
 			json+='{'
 			for (let j = 0; j < cols; j++){
-				json+=`\"${headers[j] !== undefined ? headers[j]: `?${j}?`}\": \"${matrix[i][j] !== undefined ? matrix[i][j] : ''}\"${(j+1<cols)?', ':''}`;
+				json+=`\"${headers[j] !== undefined ? headers[j]: `?${j}?`}\": \"${array[i][j] !== undefined ? array[i][j] : ''}\"${(j+1<cols)?', ':''}`;
 			}
 			json+=`}${(i+1<rows)?', ':''}`
 		}
 		json += ']}'
-		console.log(json);
 		return JSON.parse(json).table
 	}
-
 	static _generate(data) {
 		const ts = new Transform({
 			transform(chunk, enc, cb) {
@@ -60,5 +72,3 @@ export class Table {
 		return result;
 	}
 }
-
-//npm install semver
