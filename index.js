e86aaa92e22a4b1ea303132b0620a70217098f03
qpyracuk <qpyracuk@gmail.com>
2023-01-13 16:36:18 +0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,122 @@
 import process from 'process';
+
+const consoleWidth = process.stdout.columns
+const MODE_FORMAT = {
+	EQUAL: 'equal',
+	FIXED: 'fixed',
+	AUTO: 'auto',
+}
+
+function getEqualTable(header, content, info){
+
+}
+
+function tableFormater(headers, data, info, mode = 'auto'){
+	switch(mode){
+		case 'auto':
+			break;
+		case 'fixed':
+			break;
+		case 'equal':
+			break;
+	}
+}
+
+function tableCore(headers, data, mode){
+	let tableInfo = getInfoTable(headers, data)
+	let columnsSize = []
+	if(tableInfo.width < consoleWidth){
+		let numberColumns = Math.max(tableInfo.headerInfo.columnsWidth.length, tableInfo.contentInfo.columnsWidth.length)
+		if (numberColumns === tableInfo.headerInfo.columnsWidth.length){
+			let rows = tableInfo.rows
+			for(let i =0; i < numberColumns; i++){
+				columnsSize.push(0)
+			}
+			for(let i = 0; i < rows; i++){
+				columnsSize[i].
+			}
+		}
+		else{
+			//Обработать ситуацию, когда заголовков не хватает
+		}
+	}
+	else{
+		if (tableInfo.header.width < consoleWidth){
+			//Сделать переносы внутри ячеек
+		}
+		else{
+			//Сделать переносы столбцов таблицы
+		}
+	}
+}
+
+function getInfoTable(headers, content){
+	let headerInfo = getInfoRow(headers)
+	let contentInfo = getInfoContent(content)
+	return{
+		width: headerInfo.width < contentInfo.width ? contentInfo.width : headerInfo.width,
+		header: headerInfo,
+		content: contentInfo,
+		allRows: [...headerInfo.columnsWidth, ...contentInfo.columnsWidth],
+		rows: headers.length + content.length,
+	}
+}
+
+function getInfoContent(content){
+	let len = content.length, rows = [], maxWidth = 0, columnsWidth = []
+	for (let i = 0 ; i < len ; i++){
+		let row = getInfoRow(content[i])
+		for (let j = row.cellsData.length; j > 0; j--) {
+			if (columnsWidth[j] === undefined) columnsWidth[j] = row.cellsData[j].width
+			else if (columnsWidth[j] < row.cellsData[j].width) columnsWidth[j] = row.cellsData[j].width	
+		}
+		if (maxWidth < row.width) maxWidth = row.width
+		rows.push(row)
+	}
+	return {
+		width:maxWidth,
+		rowsData: rows,
+		columnsWidth: columnsWidth,
+	}
+}
+
+function getInfoRow(row){
+	let len = row.length
+	let data = []
+	let maxHeight = 0, widthRow = 0
+	for(let i = 0; i < len; i++){
+		let cell = getInfoCell(row[i])
+		data.push(cell)
+		widthRow+=cell.width
+		if (maxHeight < cell.height) maxHeight = cell.height;
+	}
+	return {
+		width: widthRow,
+		height: maxHeight,
+
+		numberColumns: len,
+		cellsData: data,
+	}
+}
+
+function getInfoCell(str){
+	let str = str.split('\n'), width = 0, height = str.length
+	for (let i = 0; i < height; i++) width = (max < str[i].length?str.length: max)
+	return {
+		width: width,
+		height: height
+	}
+}
+class Cell{
+	content
+	width
+	height
+	constructor(content, width, height){
+		this.content = content
+		this.width = width
+		this.height = height
+	}
+}
 export default class Table {
 	/**
 	 * Based on data from an array of classes, returns a row containing a table
@@ -30,7 +148,7 @@ export default class Table {
 	 * @param {Array<string>} headers Column headings
 	 */
 	static showOfArray(array, headers) {
-		console.log(Table._generate(Table._arrayToClasses(array, headers)));
+		console.log(Table._generate(array, headers));
 	}
 	static _arrayToClasses(array, headers) {
 		let rows = array.length;
@@ -52,30 +170,13 @@ export default class Table {
 		json += ']}';
 		return JSON.parse(json).table;
 	}
-	static _generate(data) {
-		const ts = new Transform({
-			transform(chunk, enc, cb) {
-				cb(null, chunk);
-			},
-		});
-		const logger = new Console({ stdout: ts });
-		logger.table(data);
-		const table = (ts.read() || '').toString();
-		let result = '';
-		for (let row of table.split(/[\r\n]+/)) {
-			let r = row.replace(/[^┬]*┬/, '┌');
-			r = r.replace(/^├─*┼/, '├');
-			r = r.replace(/│[^│]*/, '');
-			r = r.replace(/^└─*┴/, '└');
-			r = r.replace(/'/g, ' ');
-			result += `${r}\n`;
-		}
+	static _generate(headers, data) {
+		let data = Table._getPretterData(headers, data)
+		
 		return result;
 	}
-	static _getPretterData(headers, data){
-		const width = process.stdout.columns
-		
-	}
+	
+	static _generateTable(headers, data)
 	static _generatePS(headers, data){
 
 	}
